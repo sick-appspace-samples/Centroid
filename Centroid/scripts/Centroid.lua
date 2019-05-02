95dad9d473af85cadf4de7f3145d21110516af92
Florian Roser <Florian.Roser@sick.de>
2019-05-02 16:48:33 +0200

    Initial commit

---
+++ Centroid/scripts/Centroid.lua
@@ -0,0 +1,70 @@
+--[[----------------------------------------------------------------------------
+
+  Application Name:
+  Centroid
+
+  Summary:
+  Finding centroids in blobs.
+
+  How to Run:
+  Starting this sample is possible either by running the app (F5) or
+  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
+  function allows debugging step-by-step after 'Engine.OnStarted' event.
+  Results can be seen in the image viewer on the DevicePage.
+  Restarting the Sample may be necessary to show images after loading the webpage.
+  To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
+  required. For example SIM4000 with latest firmware. Alternatively the Emulator
+  in AppStudio 2.3 or higher can be used.
+
+  More Information:
+  Tutorial "Algorithms - Blob Analysis".
+
+------------------------------------------------------------------------------]]
+
+--Start of Global Scope---------------------------------------------------------
+
+print('AppEngine Version: ' .. Engine.getVersion())
+
+local DELAY = 500 -- ms between visualization steps for demonstration purpose
+
+-- Creating viewer
+local viewer = View.create()
+viewer:setID('viewer2D')
+
+-- Setting up graphical overlay attributes
+local deco = View.ShapeDecoration.create()
+deco:setPointType('DOT')
+deco:setPointSize(10)
+deco:setLineColor(0, 255, 0) -- Green
+
+--End of Global Scope-----------------------------------------------------------
+
+--Start of Function and Event Scope---------------------------------------------
+
+local function main()
+  viewer:clear()
+  local img = Image.load('resources/Centroid.bmp')
+  local imageID = viewer:addImage(img)
+  viewer:present()
+  Script.sleep(DELAY) -- for demonstration purpose only
+
+  -- Finding blobs
+  local objectRegion = img:threshold(0, 150)
+  local blobs = objectRegion:findConnected(500)
+
+  -- Analyzing each blob and visualizing the result
+  for i = 1, #blobs do
+    local center = blobs[i]:getCenterOfGravity(img)
+    viewer:addShape(center, deco, nil, imageID)
+    viewer:present() -- presenting single steps
+    Script.sleep(DELAY) -- for demonstration purpose only
+  end
+  print(#blobs .. ' blobs found')
+  print('App finished.')
+end
+
+--The following registration is part of the global scope which runs once after startup
+--Registration of the 'main' function to the 'Engine.OnStarted' event
+Script.register('Engine.OnStarted', main)
+
+--End of Function and Event Scope--------------------------------------------------
